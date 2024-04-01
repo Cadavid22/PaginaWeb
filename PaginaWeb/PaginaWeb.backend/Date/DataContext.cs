84e39a2b5a1357dcdd862b8eca61c5226fbbb178
Jorge Cadavid <cadavidjorge10@gmail.com>
2024-03-31 19:57:33 -0500

    change 3

---
+++ PaginaWeb/PaginaWeb.backend/Date/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaginaWeb.Shared.Entities;
+
+
+namespace PaginaWeb.backend.Date
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MyTask> MyTasks { get; set; }
+    }
+}
